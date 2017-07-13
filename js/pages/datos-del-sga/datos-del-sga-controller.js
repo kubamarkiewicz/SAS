0a8cda18629d2fce90d58e200f11c83422375f4d
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-07-13 16:52:14 +0200

    no message

--- js/pages/datos-del-sga/datos-del-sga-controller.js
+++ js/pages/datos-del-sga/datos-del-sga-controller.js
@@ -1,6 +1,44 @@
 app.controller('DatosDelSGAController', function($scope, $rootScope, $http, $routeParams, config, ArtisterilIntervalService) {  
 
 
+    // Popup Alerts
+
+    $scope.popupAlertsData = {};
+    $scope.popupAlertsCount = 0;
+
+    $scope.getPopupAlertsData = function()
+    {
+        $http({
+            method  : 'GET',
+            url     : config.webservice.urls.data_get_popup_alerts
+         })
+        .then(function(response) {
+            $scope.popupAlertsData = {};
+            $scope.popupAlertsCount = 0;
+            for (i in response.data.get_popup_alertsResult) {
+                $scope.popupAlertsData[response.data.get_popup_alertsResult[i].Title + response.data.get_popup_alertsResult[i].Message] = response.data.get_popup_alertsResult[i];
+                $scope.popupAlertsCount += 1;
+            }
+        });
+    }
+    ArtisterilIntervalService.start($scope.getPopupAlertsData);
+
+
+    $("section.popup-alerts .open").click(function(){
+        $("section.popup-alerts").removeClass('closed');
+        ArtisterilIntervalService.start($scope.closePopupAlerts, 300000, 'closePopupAlerts', true);
+    });
+    $("section.popup-alerts .close").click(function(){
+        $scope.closePopupAlerts();
+    });
+
+    $scope.closePopupAlerts = function() {
+        $("section.popup-alerts").addClass('closed');
+        ArtisterilIntervalService.stop('closePopupAlerts');
+    }
+
+
+
     // Signals flow
 
     $scope.signalsFlowL9Data = [];
