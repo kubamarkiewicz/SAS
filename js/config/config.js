7cff518c4285639b428c6e8f8c3063fd2ad8435b
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-07-21 10:16:04 +0200

    config

---
+++ js/config/config.js
@@ -0,0 +1,11 @@
+
+/* This file has to loaded before app.js */
+
+window.config = window.config || {};
+
+window.config.config = {
+
+    "nefab_code_length" : 12,
+    "cable_code_length" : 10
+
+}
