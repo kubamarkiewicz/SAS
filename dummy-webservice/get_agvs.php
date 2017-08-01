fa59882bd57cd983e7a8e953bbbca1cd8b869c5c
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-08-01 19:08:10 +0200

    dummy agv

--- dummy-webservice/get_agvs.php
+++ dummy-webservice/get_agvs.php
@@ -3,8 +3,8 @@
     [
 		{
 			"Id"			: "1",
-			"X"				: "<?=rand(0, 1)?>",
-			"Y"				: "<?=rand(0, 2)?>",
+			"X"				: "<?=rand(50, 51)?>",
+			"Y"				: "<?=rand(50, 52)?>",
 			"Orientation"	: "<?=rand(0, 90)?>",
 			"Status" 		: "stopped",
 			"Actions"		: [
@@ -15,8 +15,8 @@
 		},
 		{
 			"Id"			: "2",
-			"X"				: "<?=rand(10, 11)?>",
-			"Y"				: "<?=rand(10, 12)?>",
+			"X"				: "<?=rand(60, 61)?>",
+			"Y"				: "<?=rand(70, 72)?>",
 			"Orientation"	: "<?=rand(0, 90)?>",
 			"Status" 		: "stopped",
 			"Actions"		: [
@@ -27,8 +27,8 @@
 		},
 		{
 			"Id"			: "3",
-			"X"				: "<?=rand(20, 21)?>",
-			"Y"				: "<?=rand(20, 22)?>",
+			"X"				: "<?=rand(70, 71)?>",
+			"Y"				: "<?=rand(90, 92)?>",
 			"Orientation"	: "<?=rand(0, 90)?>",
 			"Status" 		: "stopped",
 			"Actions"		: [
