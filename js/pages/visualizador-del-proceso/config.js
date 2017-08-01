a84a4dbf668974e64278cb08240936a000e94e68
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-08-01 17:41:37 +0200

    calculate XY

---
+++ js/pages/visualizador-del-proceso/config.js
@@ -0,0 +1,15 @@
+
+window.config = window.config || {};
+
+window.config.map = {
+
+	"min_zoom"	: 1,
+	"max_zoom"	: 6,
+
+    "meters_width"     : 70.488,   // map width [meters]
+    "meters_height"    : 36.935,   // map height [meters]
+
+    "meters_offset_x"  : 75.144,     // x axis offset [meters]
+    "meters_offset_y"  : 108.024     // y axis offset [meters]
+
+}
